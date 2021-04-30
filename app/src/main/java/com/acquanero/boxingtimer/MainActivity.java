99f55c2de9bfab443f64d5ad93c9ca3ca5ad6fed
Pablo <pabloacquafresca@gmail.com>
2021-04-30 16:26:05 -0300

    Add rest time button working

--- app/src/main/java/com/acquanero/boxingtimer/MainActivity.java
+++ app/src/main/java/com/acquanero/boxingtimer/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         roundMinutes = Integer.parseInt(round_minutes_txt.getText().toString());
 
         buttonAddTime = findViewById(R.id.button_add_time_to_round);
+        buttonRestTime = findViewById(R.id.button_rest_time_to_round);
 
         buttonAddTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,9 +82,28 @@ public class MainActivity extends AppCompatActivity {
     }
     private void restTimeToRound(){
 
-        if (roundSeconds != 0 && roundMinutes != 0){
+        if (roundSeconds > 0){
+
+            roundSeconds = roundSeconds - 1;
+
+            if (String.valueOf(roundSeconds).length() == 1){
+
+                round_seconds_txt.setText("0" + String.valueOf(roundSeconds));
+
+            } else round_seconds_txt.setText(String.valueOf(roundSeconds));
 
 
+
+        } else {
+
+            if (roundMinutes != 0){
+
+                roundMinutes = roundMinutes - 1;
+                roundSeconds = 59;
+                round_seconds_txt.setText("59");
+                round_minutes_txt.setText(String.valueOf(roundMinutes));
+
+            }
 
         }
 
