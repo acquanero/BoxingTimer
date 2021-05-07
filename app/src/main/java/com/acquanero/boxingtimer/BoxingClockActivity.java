2d3221694d0fb16580c8ec324502dbd1d9bd27a9
pablo <pabloacquafresca@gmail.com>
2021-05-07 18:26:02 -0300

    added timer to count down. clock working and round counter working. Remains to add rest time and alarm sound

--- app/src/main/java/com/acquanero/boxingtimer/BoxingClockActivity.java
+++ app/src/main/java/com/acquanero/boxingtimer/BoxingClockActivity.java
@@ -3,23 +3,39 @@ package com.acquanero.boxingtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 
+import java.util.Timer;
+
 public class BoxingClockActivity extends AppCompatActivity {
+
+    int roundNumbers;
+    int roundSeconds;
+    int roundMinutes;
+    int restSeconds;
+    int restMinutes;
+
+    int counter;
+
+    Bundle data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boxing_clock);
 
-        Bundle data = getIntent().getExtras();
+        data = getIntent().getExtras();
 
-        int roundNumbers = data.getInt("roundNumbers");
+        roundNumbers = data.getInt("roundNumbers");
+        roundSeconds = data.getInt("roundSeconds");
+        roundMinutes = data.getInt("roundMinutes");
+        restSeconds = data.getInt("restSeconds");
+        restMinutes = data.getInt("restMinutes");
 
-        int roundSeconds = data.getInt("roundSeconds");
-        int roundMinutes = data.getInt("roundMinutes");
-        int restSeconds = data.getInt("restSeconds");
-        int restMinutes = data.getInt("restMinutes");
+        counter = 1;
+
+        int totalTimeInMilisec = (roundSeconds * 1000) + ((roundMinutes * 60) * 1000);
 
         TextView roundCounter = findViewById(R.id.label_round_counter);
         roundCounter.setText("1/" + roundNumbers);
@@ -39,5 +55,73 @@ public class BoxingClockActivity extends AppCompatActivity {
 
         }
 
+        CountDownTimer miTimer = new CountDownTimer(totalTimeInMilisec, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                if (roundSeconds == 0){
+
+                    if (roundMinutes > 0){
+
+                        roundMinutes = roundMinutes - 1;
+                        roundSeconds = 60;
+
+                        if (String.valueOf(roundSeconds).length() == 1){
+
+                            roundSecondsTxt.setText("0" + roundSeconds);
+
+                        } else {
+
+                            roundSecondsTxt.setText(String.valueOf(roundSeconds));
+
+                        }
+
+                        roundMinutesTxt.setText(String.valueOf(roundMinutes));
+
+                    } else {
+
+                        this.cancel();
+
+                    }
+
+
+                }
+
+                roundSeconds = roundSeconds - 1;
+
+                if (String.valueOf(roundSeconds).length() == 1){
+
+                    roundSecondsTxt.setText("0" + roundSeconds);
+
+                } else {
+
+                    roundSecondsTxt.setText(String.valueOf(roundSeconds));
+
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                if (counter < roundNumbers) {
+
+                    counter++;
+
+                    roundCounter.setText(counter + "/" + roundNumbers);
+
+                    roundSeconds = data.getInt("roundSeconds");
+                    roundMinutes = data.getInt("roundMinutes");
+
+                    this.start();
+
+                }
+
+            }
+        };
+
+        miTimer.start();
+
     }
+
 }
